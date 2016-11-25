e72ac61016aa231e041a1ab354de674d8103cfab
Carlos Cuatin <cecg1996@gmail.com>
2016-11-25 14:19:11 -0500

    fixing normalize

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
+    <Navbar></Navbar>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello';
+import Navbar from './components/Navbar';
 
 export default {
   name: 'app',
   components: {
-    Hello,
-  },
+    Navbar
+  }
 };
 </script>
 
+<style src="normalize.css/normalize.css"></style>
 <style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,18 @@
+<template>
+  <header class="navbar">
+    <img src="../assets/logo.png">
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'navbar'
+};
+</script>
+
+<style scoped>
+.navbar {
+  height: 80px;
+  background-color: #e2e2e2;
+}
+</style>
