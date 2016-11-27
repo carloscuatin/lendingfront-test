0f82e287c39ef831d8826e6e28c459bb6eeaac41
Carlos Cuatin <cecg1996@gmail.com>
2016-11-27 12:24:56 -0500

    add box shadow to button add

--- src/components/ProductHeader.vue
+++ src/components/ProductHeader.vue
@@ -59,6 +59,7 @@ export default {
   border-radius: 50%;
   color: #ffffff;
   background-color: #ffad00;
+  box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);
 }
 
 .button:hover {
